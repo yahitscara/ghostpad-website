b16802728add23eab10e5e729f3afe89f6267f76
nesh <mctopherganesh@gmail.com>
2025-10-21 17:29:19 -0400

    Add optimized background image (563KB)

--- src/components/BackgroundContent.tsx
+++ src/components/BackgroundContent.tsx
@@ -1,15 +1,11 @@
 export default function BackgroundContent() {
   return (
     <div className="fixed inset-0 -z-10">
-      {/* Gradient placeholder - user can add optimized image later */}
-      <div className="w-full h-full bg-gradient-to-br from-purple-100 via-blue-100 to-indigo-200" />
-
-      {/* When ready, uncomment and add optimized image: */}
-      {/* <img
-        src="/ghostpad-website/trialbgpicture2.png"
+      <img
+        src="/ghostpad-website/trialbgpicture2.jpg"
         alt="background"
         className="w-full h-full object-cover"
-      /> */}
+      />
     </div>
   );
 }
